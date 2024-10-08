67e3312cf254afa05bb18574fdc813090a5ab86c
Ameena202 <ameenanowshad222@gmail.com>
2024-10-08 12:07:33 +0530

    this is for add

--- file1.py
+++ file1.py
@@ -5,10 +5,12 @@ def add_numbers(num1, num2):
 
 # Input from the user
 number1 = float(input("Enter the first number: "))
-number2 = float(input("Enter the second number: "))
+number2 = float(input("Enter the 2nd number: "))
 
 # Adding the two numbers
 result = add_numbers(number1, number2)
 
 # Displaying the result
 print(f"The sum of {number1} and {number2} is {result}.")
+
+
