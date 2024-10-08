204454ccb5cad6fbedb87e280f73f9c5745e1c63
Ameena202 <ameenanowshad222@gmail.com>
2024-10-08 12:09:02 +0530

    this for addision in feature branch

--- file1.py
+++ file1.py
@@ -4,7 +4,7 @@ def add_numbers(num1, num2):
     return num1 + num2
 
 # Input from the user
-number1 = float(input("Enter the first number: "))
+number1 = float(input("Enter the 1st number: "))
 number2 = float(input("Enter the 2nd number: "))
 
 # Adding the two numbers
@@ -12,5 +12,3 @@ result = add_numbers(number1, number2)
 
 # Displaying the result
 print(f"The sum of {number1} and {number2} is {result}.")
-
-
