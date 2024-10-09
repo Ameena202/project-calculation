70eab68b5bc0ac047fdc184fce4f781d161691ad
Ameena202 <ameenanowshad222@gmail.com>
2024-10-09 10:40:04 +0530

    version 1 for sqaure root

---
+++ square.py
@@ -0,0 +1,13 @@
+import math
+
+# Define the two numbers
+number1 = 16
+number2 = 25
+
+# Calculate the square roots
+sqrt1 = math.sqrt(number1)
+sqrt2 = math.sqrt(number2)
+
+# Print the results
+print(f"The square root of {number1} is {sqrt1}")
+print(f"The square root of {number2} is {sqrt2}")
